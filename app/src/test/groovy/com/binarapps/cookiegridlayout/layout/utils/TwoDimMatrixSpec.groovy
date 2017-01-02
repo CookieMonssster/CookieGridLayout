089c3070f52f033e57428a99f4477afbad0c55f4
Marta Cabaj <m.cabaj@binarapps.com>
2017-01-02 15:32:16 +0100

    add first test

--- app/src/test/groovy/com/binarapps/cookiegridlayout/layout/utils/TwoDimMatrixSpec.groovy
+++ app/src/test/groovy/com/binarapps/cookiegridlayout/layout/utils/TwoDimMatrixSpec.groovy
@@ -1,9 +1,9 @@
 package com.binarapps.cookiegridlayout.layout.utils
 
 import com.binarapps.cookiegridlayout.BuildConfig
-import com.binarapps.cookiegridlayout.layout.utils.TwoDimMatrix
 import org.robolectric.annotation.Config
 import pl.polidea.robospock.RoboSpecification
+import spock.lang.Shared
 import spock.lang.Unroll
 
 /**
@@ -12,13 +12,14 @@ import spock.lang.Unroll
 @Config(manifest = "./src/main/AndroidManifest.xml", constants = BuildConfig, sdk = 18)
 public class TwoDimMatrixSpec extends RoboSpecification {
 
-    TwoDimMatrix twoDimMatrix;
+    @Shared
+    TwoDimMatrix twoDimMatrix
 
-    def setup() {
-        twoDimMatrix = new TwoDimMatrix(4)
+    def setupSpec() {
+        twoDimMatrix = new TwoDimMatrix(4);
     }
 
-
+    @Unroll
     def "Adding new element to the layout"() {
         when:
         def point = twoDimMatrix.addNewElement(spanColumns, spanRows)
@@ -28,8 +29,8 @@ public class TwoDimMatrixSpec extends RoboSpecification {
         where:
         spanColumns | spanRows || x | y
         1           | 1        || 0 | 0
-        3           | 3        || 0 | 1
-        1           | 2        || 1 | 1
+        3           | 3        || 1 | 0
+        1           | 2        || 0 | 1
         3           | 2        || 0 | 3
         2           | 1        || 0 | 5
     }
